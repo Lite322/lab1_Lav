6894c6055c7f12b04cae12f78f6eef8262a1bdd0
Valentin <rishat-rus@mail.ru>
2022-05-26 11:46:07 +0400

    asd

---
+++ Formuly.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_Lav
+{
+    public class Formuly
+    {
+        /// <summary>
+        /// Формула z1 = (cos(α) - cos(β))^2 - (sin(α) - sin(β))^2
+        /// </summary>
+        /// <param name="a"> Угол α в радианах </param>
+        /// <param name="b"> Угол β в радианах </param>
+        /// <returns> Результат формулы </returns>
+        public static double Form1(double a, double b)
+        {
+            double a1 = Formuly.ToRad(a);
+            double b1 = Formuly.ToRad(b);
+            double cosa = Math.Cos(a1);
+            double cosb = Math.Cos(b1);
+            double sina = Math.Sin(a1);
+            double sinb = Math.Cos(b1);
+            double braser1 = Math.Pow(cosa - cosb, 2);
+            double braser2 = Math.Pow(sina - sinb, 2);
+            double z = braser1 - braser2;
+            return z;
+        }
+
+        /// <summary>
+        /// Формула z2 = -4sin^2((α-β)/2) * cos(α + β)
+        /// </summary>
+        /// <param name="a"> Угол α в радианах </param>
+        /// <param name="b"> Угол β в радианах </param>
+        /// <returns> Результат формулы </returns>
+        public static double Form2(double a, double b)
+        {
+            double braser1 = Formuly.ToRad((a - b) / 2);
+            double sin = Math.Pow(Math.Sin(braser1), 2) * (-4);
+            double cos = Math.Cos(Formuly.ToRad(a + b));
+            double z = sin * cos;
+            return z;
+        }
+
+        /// <summary>
+        /// Перевод угла из градусов в радианы
+        /// </summary>
+        /// <param name="angle"> Угол в градусах </param>
+        /// <returns> Угол в радианах </returns>
+        public static double ToRad(double angle)
+        {
+            return angle * Math.PI / 180.0;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using lab1_Lav;
+using System;
 
-namespace lab1_Lav
+namespace Lab1
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Данная программа даёт решение двух формул по заданным углам");
+            Console.WriteLine("Формулы: z1 = (cos(a) - cos(b))^2 - (sin(a) - sin(b))^2 ");
+            Console.WriteLine("z2 = -4sin^2((a-b)/2) * cos(a + b)");
+            Console.WriteLine("Введите угол a:");
+            double a = Convert.ToInt32(Console.ReadLine()); //угол α в градусах
+            Console.WriteLine("Введите угол b:");
+            double b = Convert.ToInt32(Console.ReadLine()); //угол β в градусах
+            Console.WriteLine("Результат первой формулы");
+            double z1 = Formuly.Form1(a, b); //первая формула
+            double z11 = Math.Round(z1, 3); //округление до 3 знаков после запятой
+            Console.WriteLine(z11);
+            Console.WriteLine("Результат второй формулы");
+            double z2 = Formuly.Form2(a, b); //вторая формула
+            double z22 = Math.Round(z2, 3); //округление до 3 знаков после запятой
+            Console.WriteLine(z22);
+            Console.ReadLine();
         }
     }
 }
